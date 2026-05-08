a1d14cd45d5e773caf6246b83d5662c3b6877a9d
Gokul <gokulakrishnanvgl05@gmail.com>
2026-05-08 19:33:45 +0530

    fix: switched to HashRouter to resolve static 404 errors

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { BrowserRouter, Routes, Route } from "react-router";
+import { HashRouter, Routes, Route } from "react-router";
 import { Layout } from "./components/Layout";
 import { Home } from "./pages/Home";
 import { Detail } from "./pages/Detail";
@@ -12,7 +12,7 @@ import { Watchlist } from "./pages/Watchlist";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
@@ -23,7 +23,7 @@ export default function App() {
           <Route path="*" element={<Home />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
